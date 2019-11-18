02ea875d1dd524a7d4586aa6559dd60cbc45c36a
tugbacevik <tugbacevik@users.noreply.github.com>
2019-11-18 14:47:02 +0300

    UserBLL'e başlandı

--- NoteBoard/NoteBoard.BLL/UserController.cs
+++ NoteBoard/NoteBoard.BLL/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NoteBoard.DAL.Repositories;
+using NoteBoard.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace NoteBoard.BLL
 {
-    class UserController
+   public class UserController
     {
+        UserDAL _userDAL;
+        public UserController()
+        {
+            _userDAL = new UserDAL();
+        }
+        public bool Add(User user)
+        {
+            return _userDAL.Add(user) > 0;
+        }
+        //admin onay verirse yayınlanacak. 
     }
 }
